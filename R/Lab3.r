ceced843846bb77fe375cd68f432866b49646b4a
Lakshidaa <lakshidaa.s@gmail.com>
2018-09-13 09:02:42 +0200

    Done with euclidean - L

---
+++ R/Lab3.r
@@ -0,0 +1,16 @@
+
+#1.1 Write the R code
+
+#1.1.1 euclidean()
+euclidean<-function(n1,n2){
+  stopifnot(is.numeric(n1) && is.numeric(n2) && length(n1)==1 && length(n2)==1)
+
+  temp <- 0
+
+  while(!(n2==0)){
+    temp <- n2
+    n2 <- n1%%n2
+    n1 <- temp
+  }
+  n1
+}
